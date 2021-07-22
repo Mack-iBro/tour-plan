4109056825dbb4dfb0f715e2b8ded5fe213bac67
Mack <mackchannel1337@gmail.com>
2021-07-22 23:00:55 +0300

    create mobile menu

--- js/main.js
+++ js/main.js
@@ -13,6 +13,9 @@ $( document ).ready(function()  {
     enabled: true,
     onlyInViewport: false,
   },
+
+  
+
 });
 const reviewsSlider = new Swiper('.reviews-slider', {
   // Optional parameters
@@ -29,5 +32,12 @@ const reviewsSlider = new Swiper('.reviews-slider', {
   },
 });
 $('.newsletter').parallax();
-    
+});
+
+var menuButton = document.querySelector(".menu-button")
+menuButton.addEventListener("click", function() {
+  console.log("Клик по кнопке меню")
+  document
+  .querySelector(".navbar-bottom")
+  .classList.toggle("navbar-bottom--visible");
 });
